96ee9c15e674f0f7bc582dcb8d4aecb8a4068214
AlondraCordoba <alondracordoba0720@gmail.com>
2021-08-16 15:30:48 -0500

    Second commit: login

--- src/app/pages/collaborator/login/login.component.ts
+++ src/app/pages/collaborator/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email: string = '';
+  password: string = '';
+  
+  constructor( private router: Router) { }
 
-  constructor() { }
+  goHome(){ this.router.navigate(['/home']);}
+  logIn(){
+    if(this.email == 'admin@arkus.com' && this.password=='pass1234'){
+      this.router.navigate(['/homeCollaborator'])
+    }
+  }
 
   ngOnInit(): void {
   }
 
 }
+ 
