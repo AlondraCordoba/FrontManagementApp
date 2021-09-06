5b6728dc2cf07d5c1ed4a3648ee3275b4bb7067b
AlondraCordoba <alondracordoba0720@gmail.com>
2021-09-06 10:32:08 -0500

    Commit: User model modified, styles and form (add - modify) modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,13 @@ import { DashboardVisitorComponent } from './pages/visitor/dashboard-visitor/das
 import { DetailsIncomeComponent } from './pages/visitor/details-income/details-income.component';
 import { ReportsVisitorComponent } from './pages/visitor/reports-visitor/reports-visitor.component';
 
-import { HomeCollaboratorComponent } from './pages/collaborator/home-collaborator/home-collaborator.component';
-import { LoginComponent } from './pages/collaborator/login/login.component';
-import { IncomesComponent } from './pages/collaborator/incomes/incomes.component';
-import { DashboardCollaboratorComponent } from './pages/collaborator/dashboard-collaborator/dashboard-collaborator.component';
-import { TestsComponent } from './pages/collaborator/tests/tests.component';
-import { InfoFilesComponent } from './pages/collaborator/info-files/info-files.component';
-import { ReportsCollabComponent } from './pages/collaborator/reports-collab/reports-collab.component';
+import { HomeCollaboratorComponent } from './pages/coordinator/home-collaborator/home-collaborator.component';
+import { LoginComponent } from './pages/coordinator/login/login.component';
+import { IncomesComponent } from './pages/coordinator/incomes/incomes.component';
+import { DashboardCollaboratorComponent } from './pages/coordinator/dashboard-collaborator/dashboard-collaborator.component';
+import { TestsComponent } from './pages/coordinator/tests/tests.component';
+import { InfoFilesComponent } from './pages/coordinator/info-files/info-files.component';
+import { ReportsCollabComponent } from './pages/coordinator/reports-collab/reports-collab.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
